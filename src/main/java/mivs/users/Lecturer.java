6a88dc6f9501838d7cc331af37a4e825bf62064f
TomasJ <jegelevicius@gmail.com>
2018-01-13 00:12:29 +0200

    Added LecturerUI

--- src/main/java/mivs/users/Lecturer.java
+++ src/main/java/mivs/users/Lecturer.java
@@ -1,8 +1,16 @@
 package mivs.users;
 
+import java.time.LocalDate;
+
 public class Lecturer extends User {
 
     private String lecturerCode;
+    private int personalNumber;
+    private LocalDate dateOfBirth;
+    private String  email;
+    private int mobileNumber;
+    private Gender gender;
+    private String address;
 
     public Lecturer(String username, String password, Role role, String firstName, String secondName, String lecturerCode) {
         super(username, password, role, firstName, secondName);
@@ -15,5 +23,53 @@ public class Lecturer extends User {
 
     public void setLecturerCode(String lecturerCode) {
         this.lecturerCode = lecturerCode;
+    }
+
+    public int getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(int personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(int mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
