eb49076eb0fe8acc6a5116e073e02abb2639f79f
TomasJ <jegelevicius@gmail.com>
2018-01-21 12:34:50 +0200

    JavaFx lecturerScene 95%done

--- src/main/java/mivs/application/controllers/LecturerController.java
+++ src/main/java/mivs/application/controllers/LecturerController.java
@@ -9,13 +9,11 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import mivs.courses.Course;
-import mivs.users.Admin;
-import mivs.users.Gender;
-import mivs.users.Lecturer;
-import mivs.users.Student;
+import mivs.users.*;
 import mivs.utils.IOUtils;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -63,6 +61,24 @@ public class LecturerController extends Controller {
     private TableView coursesTable;
     @FXML
     private Pane viewMyCousesPane;
+    @FXML
+    private Pane viewStudentsPane;
+    @FXML
+    private TableView courseList;
+    @FXML
+    private TableColumn courseCodeCol;
+    @FXML
+    private TableColumn courseTitleCol;
+    @FXML
+    private TableColumn courseStartDateCol;
+    @FXML
+    private TableView studentList;
+    @FXML
+    private TableColumn studentCodeCol;
+    @FXML
+    private TableColumn studentFirstNameCol;
+    @FXML
+    private TableColumn studentSecondNameCol;
 
 
     private Lecturer lecturer;
@@ -125,6 +141,7 @@ public class LecturerController extends Controller {
         startPane.setVisible(false);
         viewPane.setVisible(false);
         viewMyCousesPane.setVisible(false);
+        viewStudentsPane.setVisible(false);
 
     }
 
@@ -220,7 +237,9 @@ public class LecturerController extends Controller {
         creditsCol.setCellValueFactory(new PropertyValueFactory("credit"));
         lcodeCol.setCellValueFactory(new PropertyValueFactory("lecturerCode"));
         descriptionCol.setCellValueFactory(new PropertyValueFactory("description"));
-        coursesTable.getColumns().setAll(codeCol, titleCol, startDateCol, creditsCol, lcodeCol, descriptionCol);
+        coursesTable.getColumns().setAll(codeCol, titleCol, startDateCol, creditsCol, lcodeCol,descriptionCol);
+
+
 
     }
 
@@ -248,4 +267,73 @@ public class LecturerController extends Controller {
         }
         return courses;
     }
+
+    public void viewStudentsPane(){
+
+        makePaneInvisible();
+        viewStudentsPane.setVisible(true);
+        viewMyCourseList(myCourseList());
+    }
+
+    public  void selectCourse (){
+        if (courseList.getSelectionModel().getSelectedItem()!= null){
+            Course selectedCourse = (Course) courseList.getSelectionModel().getSelectedItem();
+            viewallStudentsList(allStudentsList(selectedCourse.getCode()));
+        }
+
+
+    }
+
+    private void viewMyCourseList(ObservableList data) {
+
+        courseList.setItems(data);
+
+        courseCodeCol.setCellValueFactory(new PropertyValueFactory("code"));
+        courseTitleCol.setCellValueFactory(new PropertyValueFactory("tittle"));
+        courseStartDateCol.setCellValueFactory(new PropertyValueFactory("startDate"));
+        courseList.getColumns().setAll(courseCodeCol, courseTitleCol, courseStartDateCol);
+
+
+
+    }
+
+    private ObservableList<Student> allStudentsList(String code) {
+        ObservableList<Student> students = FXCollections.observableArrayList();
+
+
+        try {
+            HashMap<String, User> readUser = (HashMap<String, User>) IOUtils.readObjectFromFile("files/users");
+
+            for (Map.Entry<String, User> entry : readUser.entrySet()) {
+                User value = entry.getValue();
+
+                if (value.getRole().equals(Role.STUDENT)){
+                    Student st = (Student) readUser.get(value.getUsername());
+                    if(st.getRunningCourses().contains(code)){
+                        students.add(new Student(st.getUsername(),st.getPassword(),st.getRole(),st.getFirstName(),st.getSecondName(),st.getStudentCode()));
+                    }
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+    private void viewallStudentsList(ObservableList data) {
+
+        studentList.setItems(data);
+
+        studentCodeCol.setCellValueFactory(new PropertyValueFactory("studentCode"));
+        studentFirstNameCol.setCellValueFactory(new PropertyValueFactory("firstName"));
+        studentSecondNameCol.setCellValueFactory(new PropertyValueFactory("secondName"));
+        studentList.getColumns().setAll(studentCodeCol, studentFirstNameCol, studentSecondNameCol);
+
+
+
+    }
+
+
+
 }
